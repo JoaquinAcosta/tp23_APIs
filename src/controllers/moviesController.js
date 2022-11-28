3b7cadb40fda1848b049046e4c0018ee598d9896
JoaquinAcosta <joaquinnof14@gmail.com>
2022-11-27 21:02:51 -0300

    add tp terminado

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -199,34 +199,60 @@ const moviesController = {
     }
 
     },
-
-    update: function (req,res) {
+    edit: async (req, res) => {
         let movieId = req.params.id;
-        Movies
-        .update(
-            {
-                title: req.body.title,
-                rating: req.body.rating,
-                awards: req.body.awards,
-                release_date: req.body.release_date,
-                length: req.body.length,
-                genre_id: req.body.genre_id
-            },
-            {
-                where: {id: movieId}
-            })
-        .then(()=> {
-            return res.redirect('/movies')})            
-        .catch(error => res.send(error))
-    },
-    destroy: function (req,res) {
+        let promMovies = Movies.findByPk(movieId, { include: ["genre", "actors"] });
+        let promGenres = Genres.findAll();
+        let promActors = Actors.findAll();
+        Promise.all([promMovies, promGenres, promActors])
+          .then(([Movie, allGenres, allActors]) => {
+            Movie.release_date = moment(Movie.release_date).format("L");
+            return res.render(
+              path.resolve(__dirname, "..", "views", "moviesEdit"),
+              { Movie, allGenres, allActors }
+            );
+          })
+          .catch((error) => res.send(error));
+      },
+      update: function (req, res) {
         let movieId = req.params.id;
-        Movies
-        .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
-        .then(()=>{
-            return res.redirect('/movies')})
-        .catch(error => res.send(error)) 
-    }
+        Movies.update(
+          {
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            release_date: req.body.release_date,
+            length: req.body.length,
+            genre_id: req.body.genre_id,
+          },
+          {
+            where: { id: movieId },
+          }
+        )
+          .then(() => {
+            return res.redirect("/movies");
+          })
+          .catch((error) => res.send(error));
+      },
+      delete: function (req, res) {
+        let movieId = req.params.id;
+        Movies.findByPk(movieId)
+          .then((Movie) => {
+            return res.render(
+              path.resolve(__dirname, "..", "views", "moviesDelete"),
+              { Movie }
+            );
+          })
+          .catch((error) => res.send(error));
+      },
+      destroy: function (req, res) {
+        let movieId = req.params.id;
+        Movies.destroy({ where: { id: movieId }, force: true }) // force: true es para asegurar que se ejecute la acción
+          .then(() => {
+            return res.redirect("/movies");
+          })
+          .catch((error) => res.send(error));
+      }
 }
 
 module.exports = moviesController;
